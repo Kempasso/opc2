d1d02e32abd9669cf95a6f03ddfee04059130832
Alexander Tugai <aleksandr.tugay.02@gmail.com>
2023-02-08 00:36:52 +0300

    Документация, немного правок

--- tests/test_data_comparator.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from data_comparator import DataComparator
-
-
-# @pytest.mark.asyncio
-# async def test_create_codes_from_file_for_device():
-#     await DataComparator.create_base_devices()
